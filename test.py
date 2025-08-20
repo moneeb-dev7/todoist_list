cf9ec427aa98e15b4675b1c319940d53ba8c7c28
moneeb-dev7 <moneebmohammed.16@gmail.com>
2025-08-20 21:58:34 +0200

    changes user task

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
 print("Welcome to the task list app")
 tasks = []
+
+
 def mine():
-    tas= '''1- add task 
+    tas = '''1- add task 
 2-make task complite 
 3-view your to-do list
 4-quit'''
     print(tas)
     while True:
-        task= input("please choose one of this [1 / 2 / 3 / 4] : ")
+        task = input("please choose one of this [1 / 2 / 3 / 4] : ")
         if task == "1":
             add_task()
         elif task == "2":
@@ -18,26 +20,35 @@ def mine():
             break
         else:
             print("wrong choose")
+
+
 def add_task():
     user_task = input("enter your task : ")
-    com_task = {"user_task":user_task , "complite" : False }
+    com_task = {"user_task": user_task, "complite": False}
     tasks.append(com_task)
     print("task had been add")
+
+
 def compile_task():
-    non_complite= [user_task for user_task in tasks if user_task ["complite"]==False]
+    non_complite = [
+        user_task for user_task in tasks if user_task["complite"] == False]
     if non_complite:
-        for i , user_task in enumerate(non_complite):
-            print(f"{i+1}- {user_task['user_task']}")
+        for i, user_task in enumerate(non_complite):
+            print(f"{i+1}- {user_task['uer_task']}")
             print('-'*30)
         user_com = int(input("which task you had complite : "))
         non_complite[user_com-1]["complite"] = True
     else:
         print("it's empty")
+
+
 def viwe_task():
     if not tasks:
         print("no tasks")
         return
-    for i , user_task in enumerate(tasks):
-        set = "✔️" if user_task ["complite"]==True else "❌"
+    for i, user_task in enumerate(tasks):
+        set = "✔️" if user_task["complite"] == True else "❌"
         print(f"{i+1}. {user_task['user_task']}{set}")
+
+
 mine()
